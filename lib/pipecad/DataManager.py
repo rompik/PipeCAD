bbf240e081f0e5a7845de7d0315d907e0b17f66c
Roman PARYGIN <roman.parygin@gmail.com>
2026-03-17 11:14:40 +0100

    New Module Data Manager

---
+++ lib/pipecad/DataManager.py
@@ -0,0 +1,119 @@
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :: Welcome to PipeCAD!                                      ::
+# ::  ____                        ____     ______  ____       ::
+# :: /\  _`\   __                /\  _`\  /\  _  \/\  _`\     ::
+# :: \ \ \L\ \/\_\  _____      __\ \ \/\_\\ \ \L\ \ \ \/\ \   ::
+# ::  \ \ ,__/\/\ \/\ '__`\  /'__`\ \ \/_/_\ \  __ \ \ \ \ \  ::
+# ::   \ \ \/  \ \ \ \ \L\ \/\  __/\ \ \L\ \\ \ \/\ \ \ \_\ \ ::
+# ::    \ \_\   \ \_\ \ ,__/\ \____\\ \____/ \ \_\ \_\ \____/ ::
+# ::     \/_/    \/_/\ \ \/  \/____/ \/___/   \/_/\/_/\/___/  ::
+# ::                  \ \_\                                   ::
+# ::                   \/_/                                   ::
+# ::                                                          ::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# PipeCAD - Piping Design Software.
+# Copyright (C) 2021 Wuhan OCADE IT. Co., Ltd.
+# Author: Shing Liu(eryar@163.com)
+# Date: 21:16 2021-09-16
+
+from PythonQt.QtCore import *
+from PythonQt.QtGui import *
+from pipecad import *
+
+class DataManager(QWidget):
+    """docstring for DataManager"""
+    def __init__(self, parent = None):
+        QWidget.__init__(self, parent)
+        self.setupUi()
+
+    def setupUi(self):
+        self.setWindowTitle(QT_TRANSLATE_NOOP("DataManager", "Data Manager"))
+        self.verticalLayout = QVBoxLayout(self)
+
+        self.horizontalLayout = QHBoxLayout()
+
+        self.labelType = QLabel(QT_TRANSLATE_NOOP("DataManager", "Type"))
+        self.comboType = QComboBox()
+        self.comboType.addItems(["ALL", "SITE", "ZONE", "EQUI", "PIPE", "BRAN", "STRU", "FRMW", "SBFR"])
+        self.horizontalLayout.addWidget(self.labelType)
+
+        self.labelFilter = QLabel(QT_TRANSLATE_NOOP("DataManager", "Filter"))
+        self.lineFilter = QLineEdit()
+        self.lineFilter.setPlaceholderText(QT_TRANSLATE_NOOP("DataManager", "Name Filter"))
+        self.horizontalLayout.addWidget(self.labelFilter)
+
+        self.buttonRefresh = QPushButton(QT_TRANSLATE_NOOP("DataManager", "Refresh"))
+        self.buttonRefresh.clicked.connect(self.refresh)
+        self.horizontalLayout.addWidget(self.buttonRefresh)
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setHorizontalHeaderLabels([
+            QT_TRANSLATE_NOOP("DataManager", "Name (Tag)"),
+            QT_TRANSLATE_NOOP("DataManager", "Type"),
+            QT_TRANSLATE_NOOP("DataManager", "Description")
+        ])
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.verticalLayout.addWidget(self.tableWidget)
+
+        self.actionLayout = QHBoxLayout()
+
+        self.buttonModify = QPushButton(QT_TRANSLATE_NOOP("DataManager", "Modify Tag"))
+        self.buttonModify.clicked.connect(self.modifyTag)
+        self.actionLayout.addWidget(self.buttonModify)
+
+        self.verticalLayout.addLayout(self.actionLayout)
+
+    def refresh(self):
+        self.tableWidget.setRowCount(0)
+        aType = self.comboType.currentText
+        aFilter = self.lineFilter.text
+
+        aItems = []
+        if aType == "ALL":
+            for aTypeKey in ["EQUI", "PIPE", "STRU"]:
+                aItems.extend(PipeCad.CollectItem(aTypeKey))
+        else:
+            aItems = PipeCad.CollectItem(aType)
+
+        aRow = 0
+        for aItem in aItems:
+            if aFilter and aFilter not in aItem.Name:
+                continue
+
+            self.tableWidget.insertRow(aRow)
+
+            aNameItem = QTableWidgetItem(aItem.Name)
+            aNameItem.setData(Qt.UserRole, aItem)
+
+            self.tableWidget.setItem(aRow, 0, aNameItem)
+            self.tableWidget.setItem(aRow, 1, QTableWidgetItem(aItem.Type))
+            self.tableWidget.setItem(aRow, 2, QTableWidgetItem(aItem.Description))
+            aRow += 1
+
+    def modifyTag(self):
+        aRow = self.tableWidget.currentRow()
+        if aRow < 0:
+            QMessageBox.warning(self, QT_TRANSLATE_NOOP("DataManager", "Warning"), QT_TRANSLATE_NOOP("DataManager", "Please select an item first!"))
+            return
+
+        aItem = self.tableWidget.item(aRow, 0).data(Qt.UserRole)
+        if aItem is None:
+            return
+
+        aNewName, aOk = QInputDialog.getText(self, QT_TRANSLATE_NOOP("DataManager", "Modify Tag"), QT_TRANSLATE_NOOP("DataManager", "New Name:"), QLineEdit.Normal, aItem.Name)
+        if aOk and aNewName:
+            try:
+                PipeCad.StartTransaction("Modify Tag")
+                aItem.Name = aNewName
+                PipeCad.CommitTransaction()
+                # Update table
+                self.tableWidget.item(aRow, 0).setText(aNewName)
+            except Exception as e:
+                print(e)
+                QMessageBox.critical(self, QT_TRANSLATE_NOOP("DataManager", "Error"), str(e))
--- lib/pipecad/Login.py
+++ lib/pipecad/Login.py
@@ -20,6 +20,7 @@
 from PythonQt.QtCore import *
 from PythonQt.QtGui import *
 from pipecad import *
+from pipecad.DataManager import DataManager
 import omp
 import omp.newproject
 
@@ -30,33 +31,33 @@ import sys
 
 
 class LoginDialog(QDialog):
-   
-    def __init__(self, parent = None):    
+
+    def __init__(self, parent = None):
         QDialog.__init__(self, parent)
-        
+
         self.selectedProject = None
         self.projectDict = dict()
         self.projectIndex = 0
         project_users = []
-              
-          
+
+
         self.setupUi()
     # __init__
 
-    def setupUi( self ):     
+    def setupUi( self ):
         self.resize(350, 300)
         self.setMaximumWidth(350)
         self.setMaximumHeight(300)
         self.setWindowTitle(QT_TRANSLATE_NOOP("Login", "PipeCAD Login"))
 
         aCurrentPath = os.path.dirname(os.path.abspath(__file__))
-        
+
         self.hBoxLayoutProjects = QHBoxLayout()
         self.hBoxLayoutProjects.setSpacing(2)
-        
+
         self.groupButtonsProject = QButtonGroup()
-        
-        self.btnProjectPrevious = QPushButton( "", self)           
+
+        self.btnProjectPrevious = QPushButton( "", self)
         self.btnProjectPrevious.setMinimumSize( 32 , 32 )
         self.btnProjectPrevious.setMaximumSize( 32 , 32 )
         #<a href="https://www.flaticon.com/free-icons/previous" title="previous icons">Previous icons created by Pixel perfect - Flaticon</a>
@@ -64,9 +65,9 @@ class LoginDialog(QDialog):
         self.btnProjectPrevious.setIconSize( QSize( 32 , 32 ) )
         self.btnProjectPrevious.setStyleSheet("border:none;")
         self.hBoxLayoutProjects.addWidget(self.btnProjectPrevious)
-       
+
         aProject = PipeCad.Projects[self.projectIndex]
-        self.btnProject = QPushButton( "", self)           
+        self.btnProject = QPushButton( "", self)
         self.btnProject.setMinimumSize( 312 , 104 )
         self.btnProject.setMaximumSize( 312 , 104 )
         #Icon downloaded from <a href="https://www.flaticon.com/free-icons/factory" title="factory icons">Factory icons created by vectorsmarket15 - Flaticon</a>
@@ -77,9 +78,9 @@ class LoginDialog(QDialog):
         self.btnProject.setObjectName( aProject.Code )
         self.btnProject.setAutoDefault(False)
 
-        self.hBoxLayoutProjects.addWidget(self.btnProject)  
-        
-        self.btnProjectNext = QPushButton( "", self)           
+        self.hBoxLayoutProjects.addWidget(self.btnProject)
+
+        self.btnProjectNext = QPushButton( "", self)
         self.btnProjectNext.setMinimumSize( 32 , 32 )
         self.btnProjectNext.setMaximumSize( 32 , 32 )
         #<a href="https://www.flaticon.com/free-icons/previous" title="previous icons">Previous icons created by Pixel perfect - Flaticon</a>
@@ -88,54 +89,56 @@ class LoginDialog(QDialog):
         self.btnProjectNext.setStyleSheet("border:none;")
         self.hBoxLayoutProjects.addWidget(self.btnProjectNext)
 
-        self.buttonCreate = QPushButton(QT_TRANSLATE_NOOP("Login", "Create New Project"))        
+        self.buttonCreate = QPushButton(QT_TRANSLATE_NOOP("Login", "Create New Project"))
+        self.buttonDataManager = QPushButton(QT_TRANSLATE_NOOP("Login", "Open Data Manager"))
+
         self.labelUsername = QLabel(QT_TRANSLATE_NOOP("Login", "Username"))
         self.comboBoxUser = QComboBox()
         self.comboBoxUser.setEditable(True)
-        
+
         self.labelPassword = QLabel(QT_TRANSLATE_NOOP("Login", "Password"))
         self.lineEditPassword = QLineEdit( self )
         self.lineEditPassword.setEchoMode(QLineEdit.Password)
-        
+
         PipeCad.SetIndicator( self.lineEditPassword )
-            
+
         self.buttonChange = QPushButton(QT_TRANSLATE_NOOP("Login", "Change"))
         self.buttonChange.setMinimumSize( 120 , 26 )
         self.buttonChange.setMaximumSize( 120 , 26 )
-        
+
         self.labelMdb = QLabel(QT_TRANSLATE_NOOP("Login", "MDB"))
         self.comboBoxMdb = QComboBox()
         self.comboBoxMdb.setEditable(True)
-        
+
         self.checkBox = QCheckBox(QT_TRANSLATE_NOOP("Login", "Read Only"))
-        
+
         self.verticalSpacer = QSpacerItem( 20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding )
-        
+
         self.btnDesign = QPushButton( "", self )
         self.btnParagon = QPushButton( "", self )
         self.btnAdmin = QPushButton( "", self )
-        
+
         self.btnDesign.setObjectName( "Design" )
         self.btnParagon.setObjectName( "Paragon" )
         self.btnAdmin.setObjectName( "Admin" )
-                
+
         #<a href="https://www.flaticon.com/free-icons/worker" title="worker icons">Worker icons created by Freepik - Flaticon</a>
         self.btnDesign.setIcon( QIcon(aCurrentPath + '/icons/login/128x128_select_design.png') )
-        
+
         #Icon downloaded from <a href="https://www.flaticon.com/free-icons/algorithm" title="algorithm icons">Algorithm icons created by Eucalyp - Flaticon</a>
         self.btnParagon.setIcon( QIcon(aCurrentPath + '/icons/login/128x128_select_paragon.png') )
-        
+
         #Icon downloaded from <a href="https://www.flaticon.com/free-icons/data-processing" title="data processing icons">Data processing icons created by Eucalyp - Flaticon</a>
         self.btnAdmin.setIcon( QIcon(aCurrentPath + '/icons/login/128x128_select_admin.png') )
-        
+
         self.btnDesign.setIconSize( QSize(96,96) )
         self.btnParagon.setIconSize( QSize(96,96) )
         self.btnAdmin.setIconSize( QSize(96,96) )
-        
+
         self.btnDesign.setToolTip(QT_TRANSLATE_NOOP("Login", 'Module <b>Design</b>'))
         self.btnParagon.setToolTip(QT_TRANSLATE_NOOP("Login", 'Module <b>Paragon</b>'))
         self.btnAdmin.setToolTip(QT_TRANSLATE_NOOP("Login", 'Module <b>Admin</b>'))
-                
+
         #Setup Widgets Layout
         self.gridLayout = QGridLayout()
         self.gridLayout.addWidget(self.labelUsername, 0, 0, 1, 1)
@@ -146,89 +149,91 @@ class LoginDialog(QDialog):
         self.gridLayout.addWidget(self.labelMdb, 2, 0, 1, 1)
         self.gridLayout.addWidget(self.comboBoxMdb, 2, 1, 1, 1)
         self.gridLayout.addWidget(self.checkBox, 2, 2, 1, 1)
-               
+
         self.horBoxLayoutModules = QHBoxLayout()
         self.horBoxLayoutModules.addWidget( self.btnDesign )
         self.horBoxLayoutModules.addWidget( self.btnParagon )
         self.horBoxLayoutModules.addWidget( self.btnAdmin )
-        
+
         self.groupButtonsModule = QButtonGroup()
         self.groupButtonsModule.addButton( self.btnDesign )
         self.groupButtonsModule.addButton( self.btnParagon )
         self.groupButtonsModule.addButton( self.btnAdmin )
-        
+
         self.verticalLayout = QVBoxLayout( self )
-        self.verticalLayout.setContentsMargins(11, 11, 11, 11)  
+        self.verticalLayout.setContentsMargins(11, 11, 11, 11)
         self.verticalLayout.addLayout(self.hBoxLayoutProjects)
         self.verticalLayout.addLayout(self.gridLayout)
         self.verticalLayout.addItem(self.verticalSpacer)
         self.verticalLayout.addLayout(self.horBoxLayoutModules)
         self.verticalLayout.addWidget(self.buttonCreate)
-        
+        self.verticalLayout.addWidget(self.buttonDataManager)
+
         self.labelSoftwareVersion = QLabel( "v. " + PipeCad.GetVersion() )
         self.labelSoftwareVersion.setAlignment(Qt.AlignRight)
         self.verticalLayout.addWidget(self.labelSoftwareVersion)
-       
+
         self.btnProjectPrevious.clicked.connect( self.setProjectPrevious )
         self.btnProjectNext.clicked.connect( self.setProjectNext )
         self.comboBoxUser.currentIndexChanged.connect( self.selectUser )
         self.buttonChange.clicked.connect( self.changePassword )
         self.groupButtonsModule.buttonClicked.connect( self.accept )
         self.buttonCreate.clicked.connect( self.createProject )
+        self.buttonDataManager.clicked.connect( self.openDataManager )
         #self.groupButtonsProject.buttonClicked.connect( self.selectProject )
-        
-        # Deactive widgets before selecting project 
+
+        # Deactive widgets before selecting project
         self.btnDesign.setEnabled(False)
         self.btnParagon.setEnabled(False)
         self.btnAdmin.setEnabled(False)
         self.btnProjectPrevious.setEnabled(False)
-        
+
         self.selectProject()
-            
+
     # setupUi
-    
+
     def setProjectNext( self ):
-        
+
         self.projectIndex = self.projectIndex + 1
-      
+
         self.selectProject()
-        
+
     def setProjectPrevious( self ):
-        
+
         self.projectIndex = self.projectIndex - 1
-                
-        self.selectProject()                    
-        
+
+        self.selectProject()
+
     def selectProject( self ):
-        
+
         if len( PipeCad.Projects ) == 0 or len( PipeCad.Projects ) == 1:
             self.btnProjectPrevious.setEnabled(False)
             self.btnProjectNext.setEnabled(False)
-            
+
         else:
             if self.projectIndex == 0:
                 self.btnProjectPrevious.setEnabled(False)
                 self.btnProjectNext.setEnabled(True)
             elif self.projectIndex > 0  and self.projectIndex < ( len( PipeCad.Projects ) - 1 ):
                 self.btnProjectPrevious.setEnabled(True)
-                self.btnProjectNext.setEnabled(True) 
-            elif self.projectIndex == ( len( PipeCad.Projects ) - 1 ):   
+                self.btnProjectNext.setEnabled(True)
+            elif self.projectIndex == ( len( PipeCad.Projects ) - 1 ):
                 self.btnProjectPrevious.setEnabled(True)
-                self.btnProjectNext.setEnabled(False) 
+                self.btnProjectNext.setEnabled(False)
 
         aProject = PipeCad.Projects[self.projectIndex]
         self.btnProject.setText(QT_TRANSLATE_NOOP("Login", "Project: %s \nCode: %s \nNumber: %s \nDescription: \n%s") % (aProject.Name, aProject.Code, aProject.Number, aProject.Description))
-        
+
         self.comboBoxUser.setEnabled(True)
         self.lineEditPassword.setEnabled(True)
         self.buttonChange.setEnabled(True)
         self.checkBox.setEnabled(True)
-        
+
         self.comboBoxUser.clear()
         self.comboBoxMdb.clear()
 
         self.selectedProject = PipeCad.Projects[self.projectIndex]
-        
+
         if self.selectedProject is None:
             return
         # if
@@ -238,7 +243,7 @@ class LoginDialog(QDialog):
         for aUser in self.selectedProject.UserList:
             self.comboBoxUser.addItem(aUser.Name)
         # for
-        
+
         if len( self.selectedProject.MdbList ) == 0:
             self.comboBoxMdb.setEnabled(False)
             self.btnDesign.setEnabled(False)
@@ -247,7 +252,7 @@ class LoginDialog(QDialog):
             self.comboBoxMdb.setEnabled(True)
             self.btnDesign.setEnabled(True)
             self.btnParagon.setEnabled(True)
-            
+
             for aMdb in self.selectedProject.MdbList:
                 self.comboBoxMdb.addItem(aMdb.Name)
             # for
@@ -263,7 +268,7 @@ class LoginDialog(QDialog):
             self.btnAdmin.setEnabled(False)
         # if
     # selectUser
-        
+
     def accept( self, theButton ):
 
         if self.selectedProject is None:
@@ -279,10 +284,15 @@ class LoginDialog(QDialog):
 
         PipeCad.Login()
     # accept
- 
+
+    def openDataManager(self):
+        self.dataManager = DataManager(self)
+        self.dataManager.show()
+    # openDataManager
+
     def createProject(self):
         #subprocess.Popen("ProjectCreation.bat")
-        #self.reject()   
+        #self.reject()
         omp.newproject.show()
     # createProject
 
@@ -297,15 +307,15 @@ class LoginDialog(QDialog):
         aPasswordDlg = PasswordDialog(self.selectedProject, aUser, self)
         aPasswordDlg.exec()
     # changePassword
-    
+
 class PasswordDialog(QDialog):
-   
+
     def __init__(self, theProject, theUser, parent = None):
         QDialog.__init__(self, parent)
 
         self.project = theProject
         self.user = theUser
-        
+
         self.setupUi()
 
     # __init__
